cc31d014ebdf8f8460e7797d0b47a9a0bc3fa6a4
shpater1234 <shpaters@hotmail.com>
2023-03-30 18:19:11 +0300

    palindrom function

---
+++ Ex01_04/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+
+namespace Ex01_04
+{
+    class Program
+    {
+        public static void Main()
+        {
+            StringBuilder s = new StringBuilder("abccba");
+            if (IsPalindrom(s))
+                Console.WriteLine("Pali");
+            else
+                Console.WriteLine("Not Pali");
+        }
+
+        public static StringBuilder GetStringFromUser()
+        {
+            return new StringBuilder("s");
+        }
+
+        public static bool IsPalindrom(StringBuilder i_Text)
+        {
+            if (i_Text.Length <= 1)
+            {
+                return true;
+            }
+            else
+            {
+                const int k_FirstLetterIndex = 0;
+                int lastLetterIndex = i_Text.Length - 1;
+                char firstLetter, lastLetter;
+
+                firstLetter = i_Text[k_FirstLetterIndex];
+                lastLetter = i_Text[lastLetterIndex];
+                i_Text.Remove(k_FirstLetterIndex, 1);
+                i_Text.Remove(lastLetterIndex - 1, 1);
+
+                return (firstLetter == lastLetter) && IsPalindrom(i_Text);
+            }
+        }
+
+    }
+}
