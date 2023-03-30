19d2f632abaefe509d2ca9b7cb623d60f7bdc278
shpater1234 <shpaters@hotmail.com>
2023-03-30 16:16:43 +0300

    merge and delete Diamond calss

--- Ex01_02/Program.cs
+++ Ex01_02/Program.cs
@@ -5,8 +5,57 @@ namespace Ex01_02
     class Program
     {
         public static void Main()
-        { 
-            Diamond.Draw(9); 
+        {
+            const int k_NumOfRowsToPrint = 9;
+
+            Draw(k_NumOfRowsToPrint); 
+        }
+
+        public static void Draw(int i_DiamondHeight)
+        {
+            const int k_NumOfStartingStars = 1;
+            int numOfStartingSpaces = i_DiamondHeight / 2;
+
+            if (i_DiamondHeight < 0)
+            {
+                Console.WriteLine("Invalid Diamond height!");
+                return;
+            }
+
+            if (i_DiamondHeight % 2 == 0)
+            {
+                i_DiamondHeight++;
+            }
+
+            drawRecursiveHelper(numOfStartingSpaces, k_NumOfStartingStars, i_DiamondHeight);
+        }
+        private static void drawRecursiveHelper(int i_NumOfSpaces, int i_NumOfStars, int i_DiamondHeight)
+        {
+            if (i_NumOfStars == i_DiamondHeight)
+            {
+                printLine(i_NumOfSpaces, i_NumOfStars);
+            }
+            else
+            {
+                printLine(i_NumOfSpaces, i_NumOfStars);
+                drawRecursiveHelper(i_NumOfSpaces - 1, i_NumOfStars + 2, i_DiamondHeight);
+                printLine(i_NumOfSpaces, i_NumOfStars);
+            }
+        }
+
+        private static void printLine(int i_NumOfSpaces, int i_NumOfStars)
+        {
+            for (int i = 0; i < i_NumOfSpaces; i++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int i = 0; i < i_NumOfStars; i++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
         }
     }
 }
