6024954d3fdaa675e19e5dbb1d875e8c685b702a
shpater1234 <shpaters@hotmail.com>
2023-03-31 20:59:08 +0300

    get user input function and CountNumberOfDigitsGraterThanLeastSignificateDigit functions

---
+++ Ex01_05/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ex01_05
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(CountNumberOfDigitsGraterThanLeastSignificateDigit((15)));
+        }
+
+        public static bool GetUserInput(out int o_UserInput)
+        {
+            Console.WriteLine("Plesae enter a number");
+            string userInput = Console.ReadLine();
+
+            return int.TryParse(userInput, out o_UserInput);
+        }
+
+        public static int CountNumberOfDigitsGraterThanLeastSignificateDigit(int i_Number)
+        {
+            int count = 0;
+            int leastSignificantDigit = i_Number % 10;
+
+            while(Math.Abs(i_Number) > 0)
+            {
+                i_Number /= 10;
+                if(i_Number % 10 > leastSignificantDigit)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
