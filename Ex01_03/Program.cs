71916b015ff6d246a2181939be1adb55c83f408f
shpater1234 <shpaters@hotmail.com>
2023-04-02 21:24:00 +0300

    names

--- Ex01_03/Program.cs
+++ Ex01_03/Program.cs
@@ -6,36 +6,36 @@ namespace Ex01_03
     {
         public static void Main()
         {
-            int numOfDiamondLines = GetNumOfDiamondLinesFromUser();
+            int numberOfDiamondLines = GetNumOfDiamondLinesFromUser();
 
-            Ex01_02.Program.DrawDiamond(numOfDiamondLines);
+            Ex01_02.Program.DrawDiamond(numberOfDiamondLines);
             System.Console.WriteLine("Please press 'Enter' to exit...");
             System.Console.ReadLine();
         }
 
         public static int GetNumOfDiamondLinesFromUser()
         {
-            int numOfDiamondLines;
+            int numberOfDiamondLines;
             string userInput;
 
             Console.WriteLine("Enter number of Diamond lines: ");
             userInput = Console.ReadLine();      
-            numOfDiamondLines = checkValidityOfInput(ref userInput);
+            numberOfDiamondLines = checkValidityOfInput(userInput);
 
-            return numOfDiamondLines;
+            return numberOfDiamondLines;
         }
 
-        private static int checkValidityOfInput(ref string io_DiamondHeight)
+        private static int checkValidityOfInput(string i_StringDiamondHeight)
         {
             int diamondHeight;
-            bool isValid = int.TryParse(io_DiamondHeight, out diamondHeight);
+            bool isValid = int.TryParse(i_StringDiamondHeight, out diamondHeight);
 
             while (!isValid || diamondHeight < 0)
             {
                 Console.WriteLine("Invalid Diamond height!");
                 Console.WriteLine("Please enter positive value: ");
-                io_DiamondHeight = Console.ReadLine();
-                isValid = int.TryParse(io_DiamondHeight, out diamondHeight);
+                i_StringDiamondHeight = Console.ReadLine();
+                isValid = int.TryParse(i_StringDiamondHeight, out diamondHeight);
             }
 
             if (diamondHeight % 2 == 0)
